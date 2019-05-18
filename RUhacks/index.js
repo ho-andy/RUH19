228c51ac838201b4419d76d25c8122faad9cd2b2
Casey <casey.smith13@yahoo.ca>
2019-05-18 11:59:41 -0400

    insert db works

--- RUhacks/index.js
+++ RUhacks/index.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+var router = express.Router();
+var bodyParser = require('body-parser')
+const connection = require('./database');
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use('/', router);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.render("index");
 });
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get('/sign-up', (req, res) => {
+  res.render("signup");
+});
+
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
+});
+
+app.post('/submit-form', (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+  const country = req.body.country;
+
+  var sql_creds = "INSERT INTO user_creds (user, password) VALUES ('" + username + "', '" + password + "')";
+  var sql_info = "INSERT INTO user_information (user, country, personality) VALUES ('" + username + "', '" + country + "', 'A')";
+
+  connection.query(sql_creds, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+  connection.query(sql_info, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+  console.log(username);
+  console.log(password);
+  console.log(country);
+  res.end()
 });
