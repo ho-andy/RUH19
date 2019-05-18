d6e034659643854f8a173157a8fcafe00d1e0743
Andy <hoandy1998@gmail.com>
2019-05-18 12:12:56 -0400

    Express + Socket

--- RUhacks/index.js
+++ RUhacks/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
 
 
@@ -7,8 +8,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
