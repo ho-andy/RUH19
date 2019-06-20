0074d5c94242f1e2cb208f01a3849e55d6f8490c
ho-andy <hoandy1998@gmail.com>
2019-06-19 21:35:09 -0400

    changes

--- RUhacks/database.js
+++ RUhacks/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 var connection = mysql.createConnection({
-    socketPath: '/cloudsql/certain-voyager-241002:us-east4:ru-hacks-2019', //GAE
-    //host: '35.236.239.48',                                                   //local
-    user: 'admin',
-    password: 'admin',
-    database: 'penpalr'
+    socketPath: '', //GAE
+    //host: '',                                                   //local
+    user: '',
+    password: '',
+    database: ''
   })
   
   connection.connect(function(err) {
